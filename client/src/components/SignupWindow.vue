c44c754763441467209300bb1a0726d3475a1330
GrujicFilipRS <filipgrujicpersonal@gmail.com>
2025-11-22 10:51:03 +0100

    added login and signup forms

--- client/src/components/SignupWindow.vue
+++ client/src/components/SignupWindow.vue
@@ -5,28 +5,74 @@ const mode = ref<'signup' | 'login'>('signup');
 </script>
 
 <template>
-    <div class="signup-window">
-        <div class="signup-top-section">
-            <div
-                :class="['signup-mode-button', mode === 'signup' ? 'mode-active' : '']"
-                @click="mode = 'signup'"
-            >
-                Sign Up
+    <div class="signup-overlay">
+        <div class="signup-window">
+            <div class="signup-top-section">
+                <button
+                    :class="['signup-mode-button', mode === 'signup' ? 'mode-active' : '']"
+                    @click="mode = 'signup'"
+                >
+                    Sign Up
+                </button>
+                <button
+                    :class="['signup-mode-button', mode === 'login' ? 'mode-active' : '']"
+                    @click="mode = 'login'"
+                >
+                    Log In
+                </button>
             </div>
-            <div
-                :class="['signup-mode-button', mode === 'login' ? 'mode-active' : '']"
-                @click="mode = 'login'"
-            >
-                Log In
+
+            <div class="signup-form" v-show="mode === 'signup'">
+                <form>
+                    <label for="username">Username:</label>
+                    <input
+                        type="text"
+                        name="username"
+                        placeholder="Please enter your username"
+                        required
+                    />
+
+                    <label for="password">Password:</label>
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Please enter your password"
+                        required
+                    />
+
+                    <label for="rptPassword">Repeat password:</label>
+                    <input
+                        type="password"
+                        name="rptPassword"
+                        placeholder="Please repeat your password"
+                        required
+                    />
+
+                    <button type="submit">Sign up</button>
+                </form>
             </div>
-        </div>
 
-        <div class="signup-form" v-show="mode === 'signup'">
+            <div class="login-form" v-show="mode === 'login'">
+                <form>
+                    <label for="username">Username:</label>
+                    <input
+                        type="text"
+                        name="username"
+                        placeholder="Please enter your username"
+                        required
+                    />
 
-        </div>
+                    <label for="password">Password:</label>
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Please enter your password"
+                        required
+                    />
 
-        <div class="login-form" v-show="mode === 'login'">
-
+                    <button type="submit">Log in</button>
+                </form>
+            </div>
         </div>
     </div>
 </template>
