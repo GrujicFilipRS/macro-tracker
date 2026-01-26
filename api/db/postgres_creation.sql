633e03971707409493363a055cb464df37ffea34
Filip Grujic <166260749+GrujicFilipRS@users.noreply.github.com>
2026-01-26 15:33:57 +0100

    Add PostgreSQL setup script for database tables
    This file contains SQL commands for creating the necessary tables for the project, including users, plans, and eaten.

---
+++ api/db/postgres_creation.sql
@@ -0,0 +1,34 @@
+-- This file is a configuration file for setting up a postgres database for this project
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username TEXT UNIQUE NOT NULL,
+  hashed_password TEXT NOT NULL
+);
+
+CREATE TABLE plans (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    owner_id INTEGER NOT NULL,
+    num_proteins INTEGER NOT NULL,
+    num_carbs INTEGER NOT NULL,
+    num_fats INTEGER NOT NULL,
+    CONSTRAINT fk_plans_owner
+        FOREIGN KEY (owner_id)
+        REFERENCES users (id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE eaten (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    food_name TEXT NOT NULL,
+    datetime_eaten TIMESTAMP NOT NULL,
+    num_proteins INTEGER NOT NULL,
+    num_carbs INTEGER NOT NULL,
+    num_fats INTEGER NOT NULL,
+    CONSTRAINT fk_eaten_user
+        FOREIGN KEY (user_id)
+        REFERENCES users (id)
+        ON DELETE CASCADE
+);
